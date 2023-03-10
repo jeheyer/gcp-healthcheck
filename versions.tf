d62255767d4b4e27d4d3bc1b9877259a8d31c493
John Heyer <jheyer@otldap.net>
2023-03-09 16:23:10 -0800

    updates?

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.3.2"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      version = ">= 4.50.0"
+      source  = "hashicorp/google"
     }
   }
 }
